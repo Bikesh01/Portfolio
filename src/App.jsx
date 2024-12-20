f9aff62dd678c59ff69de79c2194987058efb4f4
Bikesh01 <bikeshkc999@gmail.com>
2024-12-20 17:24:03 +0545

    changed browser title name to bikesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Contact from "./Components/Contact";
-
 import Gallery from "./Components/Gallery";
-
 import Hireme from "./Components/Hireme";
 import Footer from "./Components/Footer";
 import 'remixicon/fonts/remixicon.css'
@@ -14,34 +12,34 @@ import Resume from "./Components/Resume";
 import Services from "./Components/Services";
 import Work from "./Components/Work";
 
-
 function App() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.title = "Bikesh"; // Change title here
+  }, []);
+
   return (
     <Router>
-     <Navbar/>
+      <Navbar />
       <div className="">
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/Contact" element={<Contact />} />      
+            <Route path="/Contact" element={<Contact />} />
             <Route path="/Gallery" element={<Gallery />} />
-            <Route path="/Hireme" element={<Hireme/>}/>
-            <Route path="/Resume" element={<Resume/>}/>
-            <Route path="/Services" element={<Services/>}/>
-            <Route path="/Work" element={<Work/>}/>
-
+            <Route path="/Hireme" element={<Hireme />} />
+            <Route path="/Resume" element={<Resume />} />
+            <Route path="/Services" element={<Services />} />
+            <Route path="/Work" element={<Work />} />
           </Routes>
         </div>
       </div>
 
-      
       <div className="div">
         {/* <Footer/> */}
       </div>
     </Router>
-    
   );
 }
 
